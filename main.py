fffe7804dd05ae3c00e117066cf24e2585abfed6
zims <zims@inbox.lv>
2020-04-28 16:14:14 +0300

    firs steps

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+res = requests.get('https://f1.lv/2020/')
+soup = BeautifulSoup(res.text, 'html.parser')
+all_class = soup.select('.entry-title.td-module-title')
+# print(all_class[0])
+# print(soup.select())
+all_titles = soup.select('.entry-title.td-module-title')
+print(all_titles[0])
+
