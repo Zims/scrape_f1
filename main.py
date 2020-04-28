0695b9b68e2787a5e2a3794ac27c5d0e659d3edf
zims <zims@inbox.lv>
2020-04-28 17:13:37 +0300

    nope

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import requests
 res = requests.get('https://f1.lv/2020/')
 soup = BeautifulSoup(res.text, 'html.parser')
 all_class = soup.select('.entry-title.td-module-title')
-# print(all_class[0])
-# print(soup.select())
-all_titles = soup.select('.entry-title.td-module-title')
-print(all_titles[0])
+
+print(all_class[0].get('a href'))
 
